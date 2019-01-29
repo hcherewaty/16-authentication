f32a55995efa9ecab1259cfb5315d1ab67cd962a
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-01-29 06:54:25 -0800

    updates start script

--- auth-server/index.js
+++ auth-server/index.js
@@ -11,4 +11,4 @@ const options = {
 mongoose.connect(process.env.MONGODB_URI, options);
 
 // Start the web server
-require('./src/app.js').start(process.env.PORT);
+require('./src/app').start(process.env.PORT);
